a72cda00f50bd11840a01d9f89edf32d2a068a28
diegoapena <diego.apena@tecsup.edu.pe>
2026-03-21 21:32:16 -0500

    ULTIMO

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -3,43 +3,50 @@ using UnityEngine;
 public class BaseEnemy : MonoBehaviour
 {
     public float range = 10f;
+    public float moveSpeed = 3f;
     public bool InArea = false;
     Transform player;
-    
 
     void Start()
     {
         GetComponent<SphereCollider>().radius = range;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-      
-        
+        if (InArea && player != null)
+        {
+            // Mover al enemigo hacia el jugador
+            Vector3 direction = (player.position - transform.position).normalized;
+            transform.position += direction * moveSpeed * Time.deltaTime;
+        }
     }
 
-   
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             player = other.transform;
             InArea = true;
-            print("Player Entered");
-            
+        }
+        else if (other.CompareTag("Player"))
+        {
+
+            Destroy(gameObject);
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             InArea = false;
             player = null;
-            print("Player Exit");
             
         }
     }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
@@ -51,7 +58,5 @@ public class BaseEnemy : MonoBehaviour
             Vector3 dir = player.position - transform.position;
             Gizmos.DrawRay(transform.position, dir);
         }
-
-
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 moveInput;
+    public InputSystem_Actions inputSystem;
+    public float speed = 5f;
+
+    private void OnEnable()
+    {
+        inputSystem = new InputSystem_Actions();
+        inputSystem.Enable();
+
+        inputSystem.Player.Move.started += MovementBehavior;
+        inputSystem.Player.Move.performed += MovementBehavior;
+        inputSystem.Player.Move.canceled += MovementBehavior;
+    }
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+
+    private void MovementBehavior(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }   
+}
